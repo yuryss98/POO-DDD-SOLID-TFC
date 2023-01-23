ea63a562bc4113486885a3670858306314cf897f
Yury Stolarz <yurysscareer@gmail.com>
2023-01-23 13:26:30 -0300

    feat: login controller

---
+++ app/backend/src/infra/controllers/user/Login.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import LoginUseCase from '../../../domain/use-cases/user/Login.usecase';
+
+export default class LoginController {
+  constructor(private _LoginUseCase: LoginUseCase) { }
+
+  login = async (req: Request, res: Response) => {
+    const token = await this._LoginUseCase.execute(req.body);
+
+    return res.status(200).json({ token });
+  };
+}
