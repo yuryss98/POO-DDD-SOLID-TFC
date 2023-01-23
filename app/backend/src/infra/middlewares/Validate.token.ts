0f1c62b4f0c6312916f93d03d71d4832fb9a7783
Yury Stolarz <yurysscareer@gmail.com>
2023-01-23 15:29:25 -0300

    feat: validate token

---
+++ app/backend/src/infra/middlewares/Validate.token.ts
@@ -0,0 +1,23 @@
+import { Response, NextFunction, Request } from 'express';
+import { verify } from 'jsonwebtoken';
+
+const validateToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(401).json({ message: 'Token not found' });
+    }
+
+    const SECRET_KEY = process.env.JWT_SECRET as string;
+
+    const decoded = verify(token, SECRET_KEY);
+
+    req.body.user = decoded;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export default validateToken;
