9b81fd139cc06d4b4f550da3f7f85191dc390497
Yury Stolarz <yurysscareer@gmail.com>
2023-01-23 15:08:04 -0300

    feat: add login validation

--- app/backend/src/domain/use-cases/user/Login.usecase.ts
+++ app/backend/src/domain/use-cases/user/Login.usecase.ts
@@ -3,12 +3,15 @@ import { IUserCredentials } from '../../entities/interfaces/IUser';
 import HttpException from '../validations/Http.exception';
 import Password from '../validations/Decrypt.password';
 import { IAuthService } from '../../../infra/auth/Token';
+import LoginValidation from '../validations/user-validations/Login.validation';
 
 export default class LoginUseCase {
   constructor(private userRepository: UserRepository, private authService: IAuthService) { }
 
   async execute(user: IUserCredentials) {
-    const userFound = await this.userRepository.findByEmail(user.email);
+    const validateUserCredentials = new LoginValidation(user.email, user.password);
+
+    const userFound = await this.userRepository.findByEmail(validateUserCredentials.email);
     if (!userFound || !Password.checkPassword(user.password, userFound.password)) {
       throw new HttpException(401, 'Incorrect email or password');
     }
---
+++ app/backend/src/domain/use-cases/validations/user-validations/Login.validation.ts
@@ -0,0 +1,28 @@
+import { IUserCredentials } from '../../../entities/interfaces/IUser';
+import HttpException from '../Http.exception';
+
+export default class LoginValidation implements IUserCredentials {
+  constructor(private _email: string, private _password: string) {
+    this.validateUserCredentials();
+  }
+
+  validateUserCredentials() {
+    if (!this._email || !this._password) {
+      throw new HttpException(400, 'All fields must be filled');
+    }
+
+    const isValidEmail = this._email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{3})$/i);
+
+    if (!isValidEmail) {
+      throw new HttpException(401, 'Incorrect email or password');
+    }
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get password() {
+    return this._password;
+  }
+}
