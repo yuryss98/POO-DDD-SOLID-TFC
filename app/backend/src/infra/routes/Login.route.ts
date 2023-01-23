bec4413976993a325e35c28c1baa0df0cc9f1652
Yury Stolarz <yurysscareer@gmail.com>
2023-01-23 15:36:02 -0300

    feat: add endpoint /get in login/validate

--- app/backend/src/infra/routes/Login.route.ts
+++ app/backend/src/infra/routes/Login.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import LoginUseCase from '../../domain/use-cases/user/Login.usecase';
 import AuthService from '../auth/Token';
 import LoginController from '../controllers/user/Login.controller';
+import validateToken from '../middlewares/Validate.token';
 import SequelizeUserRepository from '../sequelize/repositories/User.repository';
 
 const router = Router();
@@ -11,6 +12,7 @@ const authService = new AuthService();
 const loginUseCase = new LoginUseCase(usermodel, authService);
 const loginController = new LoginController(loginUseCase);
 
+router.get('/validate', validateToken, loginController.role);
 router.post('/', loginController.login);
 
 export default router;
