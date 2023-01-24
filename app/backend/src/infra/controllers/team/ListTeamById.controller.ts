22b5b9380d1e4b467f6a1aee4dab2fc04b5f535e
Yury Stolarz <yurysscareer@gmail.com>
2023-01-24 18:12:12 -0300

    feat: add controller of list team by id

---
+++ app/backend/src/infra/controllers/team/ListTeamById.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import ListTeamByIdUseCase from '../../../domain/use-cases/team/ListTeamById.usecase';
+
+export default class ListAllTeamsController {
+  constructor(private _listTeamByIdUseCase: ListTeamByIdUseCase) { }
+
+  getById = async (req: Request, res: Response) => {
+    const team = await this._listTeamByIdUseCase.execute(Number(req.params.id));
+
+    return res.status(200).json(team);
+  };
+}
