c5298edd1609f4fb06cf056ba96d4fbe80fe5081
Yury Stolarz <yurysscareer@gmail.com>
2023-01-24 00:04:14 -0300

    test: add exception tests on login

--- app/backend/src/tests/login.spec.ts
+++ app/backend/src/tests/login.spec.ts
@@ -13,13 +13,33 @@ const { expect } = chai;
 describe('Testing login behavior', () => {
   afterEach(sinon.restore);
 
-  it('should return status 400', async () => {
+  it('Should return status 400 when not informing the email field', async () => {
     const response = await chai
     .request(app)
     .post('/login')
-    .send({ password: 'hsauhsauhdusahd' })
+    .send({ password: 'password' })
 
     expect(response.status).to.be.equal(400);
-    expect(response.body).to.deep.equal({ message: 'All fields must be filled' })
+    expect(response.body).to.deep.equal({ message: 'All fields must be filled' });
+  });
+
+  it('Should return status 400 when not informing the password field', async () => {
+    const response = await chai
+    .request(app)
+    .post('/login')
+    .send({ email: 'email@email.com' })
+
+    expect(response.status).to.be.equal(400);
+    expect(response.body).to.deep.equal({ message: 'All fields must be filled' });
+  });
+
+  it('should return status 401 when informing an email in invalid format', async () => {
+    const response = await chai
+    .request(app)
+    .post('/login')
+    .send({ email: 'emailInvalid', password: 'password' })
+
+    expect(response.status).to.be.equal(401);
+    expect(response.body).to.deep.equal({ message: 'Incorrect email or password' });
   });
 });
