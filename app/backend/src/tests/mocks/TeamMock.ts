4dfc6bc734880c0f7bd6b9e4382549fad2d2de4c
Yury Stolarz <yurysscareer@gmail.com>
2023-01-24 18:03:41 -0300

    test: add the behavior of listing all teams

---
+++ app/backend/src/tests/mocks/TeamMock.ts
@@ -0,0 +1,19 @@
+const teams = [
+  {
+    teamName: 'Avaí/Kindermann',
+  },
+  {
+    teamName: 'Bahia',
+  },
+  {
+    teamName: 'Botafogo',
+  },
+  {
+    teamName: 'Corinthians',
+  },
+  {
+    teamName: 'Cruzeiro',
+  },
+]
+
+export default teams;
---
+++ app/backend/src/tests/team.spec.ts
@@ -0,0 +1,28 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Team from '../database/models/Team'
+import TeamMock from './mocks/TeamMock';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Tests the behaviors in the teams route', () => {
+  afterEach(sinon.restore)
+
+  it('Should return status 200 and list all teams', async () => {
+    sinon.stub(Team, 'findAll').resolves(TeamMock as Team[]);
+
+    const response = await chai
+    .request(app)
+    .get('/teams')
+
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(TeamMock);
+  });
+});
