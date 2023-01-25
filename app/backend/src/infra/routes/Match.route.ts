496f8ccbb6d2999107e05ace17c1ed1e6852e692
Yury Stolarz <yurysscareer@gmail.com>
2023-01-25 19:31:14 -0300

    feat(POST /matches): add token validator

--- app/backend/src/infra/routes/Match.route.ts
+++ app/backend/src/infra/routes/Match.route.ts
@@ -3,10 +3,11 @@ import {
   listAllMatchesController,
   saveMatchController,
 } from '../factories/controllers/match.factory';
+import validateToken from '../middlewares/Validate.token';
 
 const router = Router();
 
 router.get('/', listAllMatchesController.getAll);
-router.post('/', saveMatchController.saveMatch);
+router.post('/', validateToken, saveMatchController.saveMatch);
 
 export default router;
