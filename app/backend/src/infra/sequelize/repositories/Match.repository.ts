2928719e28515687d1638f22ae5161cc450f9224
Yury Stolarz <yurysscareer@gmail.com>
2023-01-25 12:45:23 -0300

    refactor: sequelize match repository

--- app/backend/src/infra/sequelize/repositories/Match.repository.ts
+++ app/backend/src/infra/sequelize/repositories/Match.repository.ts
@@ -1,3 +1,4 @@
+import Team from '../../../database/models/Team';
 import MatchModel from '../../../database/models/Match';
 import IMatchDTO from '../../../domain/interfaces/IMatch';
 
@@ -5,8 +6,20 @@ export default class SequelizeMatchRepository {
   private modelMatch = MatchModel;
 
   async findAll() {
-    const matches = await this.modelMatch.findAll({ raw: true });
+    const matches = await this.modelMatch.findAll({
+      include: [
+        { model: Team, as: 'homeTeam', attributes: ['teamName'] },
+        { model: Team, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
 
-    return matches as IMatchDTO[];
+    const matchDataValues = matches.map((match) => match.dataValues);
+    const matchData = matchDataValues.map((data) => ({
+      ...data,
+      homeTeam: data.homeTeam.dataValues,
+      awayTeam: data.awayTeam.dataValues,
+    }));
+
+    return matchData as IMatchDTO[];
   }
 }
