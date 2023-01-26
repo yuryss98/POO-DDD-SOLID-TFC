e30fab0f27a1feab4c0d16c28142717762c39457
Yury Stolarz <yurysscareer@gmail.com>
2023-01-26 13:13:54 -0300

    fix: resolve bug in create match use case

--- app/backend/src/domain/use-cases/match/SaveMatch.usecase.ts
+++ app/backend/src/domain/use-cases/match/SaveMatch.usecase.ts
@@ -1,23 +1,27 @@
+import TeamRepository from '../../repository/Team.repository';
 import { IMatchSave } from '../../interfaces/IMatch';
 import MatchRepository from '../../repository/Match.repository';
 import HttpException from '../validations/Http.exception';
 import SaveMatchValidation from '../validations/match-validations/SaveMatch.validation';
 
 export default class SaveMatchUseCase {
-  constructor(private matchRepository: MatchRepository) { }
+  constructor(
+    private _matchRepository: MatchRepository,
+    private _teamRepository: TeamRepository,
+  ) { }
 
   async execute(match: IMatchSave) {
     SaveMatchValidation.validateFieldInput(match);
 
-    const teams = await this.matchRepository.findAll(undefined);
-    const homeTeamExists = teams.filter((team) => team.homeTeamId === match.homeTeamId);
-    const awayTeamExists = teams.filter((team) => team.awayTeamId === match.awayTeamId);
+    const teams = await this._teamRepository.findAll();
+    const homeTeamExists = teams.filter((team) => team.id === match.homeTeamId);
+    const awayTeamExists = teams.filter((team) => team.id === match.awayTeamId);
 
     if (!homeTeamExists.length || !awayTeamExists.length) {
       throw new HttpException(404, 'There is no team with such id!');
     }
 
-    const saveMatchId = await this.matchRepository.saveMatch(match);
+    const saveMatchId = await this._matchRepository.saveMatch(match);
 
     return { id: saveMatchId, ...match, inProgress: true };
   }
