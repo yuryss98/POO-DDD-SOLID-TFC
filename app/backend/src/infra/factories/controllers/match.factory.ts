913ba499798604c3d5c35b4619d0f140571946ac
Yury Stolarz <yurysscareer@gmail.com>
2023-01-26 13:19:25 -0300

    fix(POST /matches): add team repository

--- app/backend/src/infra/factories/controllers/match.factory.ts
+++ app/backend/src/infra/factories/controllers/match.factory.ts
@@ -3,12 +3,14 @@ import ListAllMatchesUseCase from '../../../domain/use-cases/match/ListAllMatche
 import ListAllMatchesController from '../../controllers/match/ListAllMatches.controller';
 import SaveMatchController from '../../controllers/match/SaveMatch.controller';
 import SaveMatchUseCase from '../../../domain/use-cases/match/SaveMatch.usecase';
+import SequelizeTeamRepository from '../../sequelize/repositories/Team.repository';
 
 const matchRepository = new SequelizeMatchRepository();
 const listAllMatchesUseCase = new ListAllMatchesUseCase(matchRepository);
 const listAllMatchesController = new ListAllMatchesController(listAllMatchesUseCase);
 
-const saveMatchUseCase = new SaveMatchUseCase(matchRepository);
+const teamRepository = new SequelizeTeamRepository();
+const saveMatchUseCase = new SaveMatchUseCase(matchRepository, teamRepository);
 const saveMatchController = new SaveMatchController(saveMatchUseCase);
 
 export { listAllMatchesController, saveMatchController };
